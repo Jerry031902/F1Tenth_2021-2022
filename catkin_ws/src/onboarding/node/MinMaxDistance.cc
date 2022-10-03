3edfa1af0749af6da0ed9423e7f0f0008af70e64
Jerry031902 <jhua2061@gmail.com>
2022-10-02 21:49:36 -0400

    working on onboarding

--- catkin_ws/src/onboarding/node/MinMaxDistance.cc
+++ catkin_ws/src/onboarding/node/MinMaxDistance.cc
@@ -2,8 +2,8 @@
 #include <ros/ros.h> 
 
 #include <onboarding/PointDist.h>
+#include <sensor_msgs/LaserScan.h>
 // (todo) Include the LaserScan message header from the sensor_msgs library
-
 
 class ScanDist
 {
@@ -19,15 +19,15 @@ public:
     ScanDist()
     {
         n = ros::NodeHandle("~");
-
+        scanSub = n.subscribe("/scan",1,scan_cb);
         // (todo) Initialize the scanSub and subscribe to 
         // the topic "/scan" with a buffer size of 1
         // using the callback scan_cb
-
+        minPub = n.advertise<onboarding::PointerDist> ("/min_point",1);
         // (todo) Initialize the minPub publisher to publish on the "/min_point" 
         // topic and with the message type of onboarding::PointDist with a 
         // queue size of 1
-        
+        maxPub = n.advertise<onboarding::PointerDist> ("/max_point",1);
         // (todo) Initialize the maxPub publisher to publish on the "/max_point"
         // topic and with the message type of onboarding::PointDist with a 
         // queue size of 1
@@ -35,13 +35,18 @@ public:
 
     void scan_cb(const sensor_msgs::LaserScan &msg)
     {
+        minPoint.distance = msg->range_min;
+        minPOint.angle = msg->angle_min;
         // (todo) Find the min value and it's angle
         // Save it to the pre-declared minPoint variable.
 
-
+        maxPoint.distance = msg->range_max;
+        maxPoint.distance = msg->angle_max;
         // (todo) Find the max value and it's angle
         // Save it to the pre-declared maxPoint variable.
-
+        minPub.publish(minPoint);
+        maxPub.publish(maxPoint);
+        //ROSINFO?
         // (todo) Publish those values to their respective
         // publishers
 
@@ -50,10 +55,9 @@ public:
 
 int main(int argc, char **argv)
 {
-    // (todo) Initialized this file as a ros node
-    
+    ros::init(argc, argv, "MinMaxDistance_Node")
+    // (todo) Initialized this file as a ros node (?)
     ScanDist sd;
     ros::spin();
-
     return 0;
 }   
